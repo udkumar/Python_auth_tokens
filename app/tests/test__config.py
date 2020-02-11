218a8def0c4537e4e45361cbcc9cfa5f5c651ca0
udkumar <udkumar@hotmail.com>
2020-02-11 13:50:35 +0530

    db and config test check

---
+++ app/tests/test__config.py
@@ -0,0 +1,23 @@
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.server.config.DevelopmentConfig')
+        return app
+
+    def test_app_is_development(self):
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertFalse(current_app is None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:@localhost/python_auth_tokens'
+        )
+
+
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.server.config.TestingConfig')
+        return app
+
+    def test_app_is_testing(self):
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'postgresql://postgres:@localhost/python_auth_tokens_test'
+        )
